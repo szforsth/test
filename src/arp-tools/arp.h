b344c9d31db68ce04ebe06e0832a014d01d7e85f
122 <122@tl-bj.com.cn>
2022-07-10 18:36:18 +0800

    init files for test

---
+++ src/arp-tools/arp.h
@@ -0,0 +1,6 @@
+#ifndef ARP_H_
+#define ARP_H_
+
+void test();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "arp-tools/arp.h"
+
+int main(int argc, char *argv[])
+{
+	test();
+	return 0;
+}
