af5390dd7f816f1f469268250419b243f3a3ca21
zsforsth <zhangshen0330@163.com>
2022-09-02 17:14:45 +0800

    update arp-tools

--- src/arp-tools/arp.c
+++ src/arp-tools/arp.c
@@ -49,7 +49,7 @@ void *send_arp_ask(void *arg)
 	unsigned char send_msg[1024] = {
 		//--------------组MAC--------14------
 		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, //dst_mac: FF:FF:FF:FF:FF:FF
-		0x00, 0x0c, 0x29, 0x6f, 0x42, 0xd2, //src_mac: 00:0c:29:75:a6:51
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, //src_mac: 00:0c:29:75:a6:51
 		0x08, 0x06,							//类型：0x0806 ARP协议
 		
 		//--------------组ip--------28-----
@@ -58,16 +58,20 @@ void *send_arp_ask(void *arg)
 		//--------------组ARP--------28-----
 		0x00, 0x01, 0x08, 0x00,				//硬件类型1(以太网地址),协议类型0x0800(IP)	
 		0x06, 0x04, 0x00, 0x01,				//硬件、协议地址分别是6、4，op:(1：arp请求，2：arp应答)
-		0x00, 0x0c, 0x29, 0x6f, 0x42, 0xd2,	//发送端的MAC地址
-		172,  18,   0,  173,  				//发送端的IP地址
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	//发送端的MAC地址
+		0,  0,   0,  0,  				//发送端的IP地址
 		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,	//目的MAC地址（由于要获取对方的MAC,所以目的MAC置零）
-		172,  20,   226,  11				//目的IP地址
+		0,  0,   0,  0				//目的IP地址
 	};
 	
 	//2.数据初始化
 	struct sockaddr_ll sll;					//原始套接字地址结构
 	struct ifreq ethreq;					//网络接口地址
-	strncpy(ethreq.ifr_name, "ens34", IFNAMSIZ);	//指定网卡名称
+	printf("Enter the NIC name:\n");
+	char nic_name[IFNAMSIZ] = {0};
+	fgets(nic_name, IFNAMSIZ, stdin);
+	nic_name[strlen(nic_name)-1] = '\0';
+	strncpy(ethreq.ifr_name, nic_name, IFNAMSIZ);	//指定网卡名称
 	
 	//3.将网络接口赋值给原始套接字地址结构
 	ioctl(sock_raw_fd, SIOCGIFINDEX, (char *)&ethreq);
@@ -85,14 +89,21 @@ void *send_arp_ask(void *arg)
 	}
 	
 	//5.获取本地机(eth0)的MAC
-	if (!(ioctl(sock_raw_fd, SIOCGIFHWADDR, (char *) &ethreq)))
-    {
+	if (!(ioctl(sock_raw_fd, SIOCGIFHWADDR, (char *)&ethreq)))
+    	{
 		for(i=0; i<6; i++)
 		{
 			//将src_mac、发送端的MAC地址组包
-			send_msg[22+i] = send_msg[6+i] = (unsigned char) ethreq.ifr_hwaddr.sa_data[i];			
+			send_msg[22+i] = send_msg[6+i] = (unsigned char)ethreq.ifr_hwaddr.sa_data[i];			
 		}
-    }
+    	}
+	char self_mac[18] = "";		//arp响应的MAC
+	char self_ip[16] = "";		//arp响应的IP
+
+	sprintf(self_mac, "%02x:%02x:%02x:%02x:%02x:%02x", \
+			send_msg[22],send_msg[23],send_msg[24],send_msg[25],send_msg[26],send_msg[27]);
+	sprintf(self_ip, "%d.%d.%d.%d", send_msg[28], send_msg[29], send_msg[30], send_msg[31]);
+	printf("selfIP:%s - selfMAC:%s\n",self_ip, self_mac);
 	
 	while(1)
 	{
@@ -101,7 +112,7 @@ void *send_arp_ask(void *arg)
 		char input_buf[1024] = "";
 		
 		//6.获取所要扫描的网段（172.20.226.0）
-		printf("input_dst_Network:172.20.226.0\n");
+		printf("Enter the target network segment:\n");
 		fgets(input_buf, sizeof(input_buf), stdin);
 		sscanf(input_buf, "%d.%d.%d.", &num[0], &num[1], &num[2]//目的IP地址 
 		);
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "arp-tools/arp.h"
+#include "netool.h"
 
 int main(int argc, char *argv[])
 {
